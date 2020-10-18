f4ea6a4c4bc6e98707d4c0aeb9957e7042f16aaf
Justin Mead <meadmiracle@gmail.com>
2020-10-18 16:33:33 -0700

    it's working, yay

--- dogs.ps1
+++ dogs.ps1
@@ -1,7 +1,30 @@
+Import-Module .\AirTable.psm1
+. .\keys.ps1
+
+#api key param = $airTableKey
+#base key param = $dogsBaseKey
+
+#script config
 $url = 'https://www.shelterluv.com/api/v3/available-animals/10032?species=Dog&embedded=1&iframeId=shelterluv_wrap_1601915424669&columns=1'
+$tableName = 'Dogs'
 
-$response = Invoke-WebRequest $url
+#fetch new dog data
+$animals = ((Invoke-WebRequest $url).Content | ConvertFrom-Json | Select-Object animals).animals | Select-Object nid, name, uniqueId, sex, weight, weight_group, age_group, breed, secondary_breed, primary_color, secondary_color
+$currentIds = $animals.uniqueId
 
-$animals = $response.Content | ConvertFrom-Json | Select-Object animals
+#get logged data from airtable
+Set-AirTableAuth $airTableKey
+$records = Get-AirTableRecords $dogsBaseKey $tableName
+$loggedIds = $records.fields.uniqueId
 
-$animals
+#check to see if there are any new dogs, and push them to airtable if so
+$newIds = (Compare-Object -ReferenceObject $loggedIds -DifferenceObject ($currentIds) | Where-Object{$_.sideIndicator -eq "=>"}).InputObject
+if ($newIds.Count -gt 0) {
+    foreach($id in $newIds) {
+        $pup = $animals | Where-Object { $_.uniqueId -eq $id }
+
+        $pup | Add-Member -NotePropertyName 'Link' -NotePropertyValue "https://www.shelterluv.com/embed/animal/$id"
+        
+        Add-AirTableRecord $dogsBaseKey $tableName $pup
+    }
+}
